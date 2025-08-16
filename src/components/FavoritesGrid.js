bbbaf72b19d45870cdfa47451712081aa303ef23
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-08-16 01:21:21 -0300

    Actualizacion favorite page

--- src/components/FavoritesGrid.js
+++ src/components/FavoritesGrid.js
@@ -1,13 +1,22 @@
+'use client'
+
+import ProductCard from "./ProductCard"
+import { useAppContext } from "@/app/context/AppContext"
+
 const FavoritesGrid = () => {
   const { favorites } = useAppContext()
 
-  if (favorites.length === 0) return <p className="text-white text-center text-2xl">No hay favoritos aún</p>
+  if (!favorites || favorites.length === 0) {
+    return <p className="text-3xl text-center px-5 font-bold text-white mb-6 py-35">Todavía no hay favoritos</p>
+  }
 
   return (
-    <section className="px-10 py-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+    <section className="px-5 md:px-20 py-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
       {favorites.map(product => (
         <ProductCard key={product.id} product={product} />
       ))}
     </section>
   )
 }
+
+export default FavoritesGrid
